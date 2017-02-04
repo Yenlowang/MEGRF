d7021c63a0f1c3ad496806160f1cf628e6b23eee
Yenlowang <Yenlowang@users.noreply.github.com>
2017-02-04 13:11:59 +0100

    Update Vasicek.R
    Inclusion de calculo de cupones 0-5
    Bono = exp(x = -colSums(r[2:(nobs+1),]*dt))
    
    Precio sigue siendo inexacto

--- Vasicek.R
+++ Vasicek.R
@@ -17,36 +17,44 @@ nobs = 300; nsims = 1000
 
 ShortRateVasicek = function(a, b, r0, v, t, nobs, nsims)
 {
-    
-    dt = t/nobs
-    
-    r = matrix(data = 0, nrow = nobs+1, ncol = nsims)
-    r[1,] = r0
-    
-    for(i in 1:nsims)
+  
+  dt = t/nobs
+  
+  r = matrix(data = 0, nrow = nobs+1, ncol = nsims)
+  r[1,] = r0
+  
+  for(i in 1:nsims)
+  {
+    for(j in 1:nobs+1)
     {
-        for(j in 1:nobs+1)
-        {
-            dr = a*(b-r[j-1,i])*dt + v*sqrt(x = dt)*rnorm(n = 1, mean = 0, sd = 1)
-            r[j,i] = r[j-1,i] + dr
-        }
+      dr = a*(b-r[j-1,i])*dt + v*sqrt(x = dt)*rnorm(n = 1, mean = 0, sd = 1)
+      r[j,i] = r[j-1,i] + dr
     }
-    
-    return(r)
+  }
+  
+  return(r)
 }
+
+##################################################
+# Para hacerlo con un solo bucle
+# for(i in 2:(m+1)){
+#   dr = a*(b-r[i-1,])*dt + v*sqrt(x = dt)*rnorm(n = n, mean = 0, sd = 1)
+#   r[i,] = r[i-1,] + dr
+# }
+##################################################
 
 r = ShortRateVasicek(a = a, b = b, r0 = r0, v = v, t = Tcanc, nobs = nobs, nsims = nsims)
 
 dt = Tcanc/nobs
-seqT = seq(0, Tcanc, dt)
+seqT = seq(from = 0, to = Tcanc, by = dt)
 
 matplot(x = seqT, y = r, type = "l", lty = 1, main = "Short Rate Vasicek Path")
 
 # Para crear el valor de los P(Tc, T)
 precioBonoCCVasicek<- function(r, a, b, v, T){
-    B <- (1-exp(-T*a))/a
-    A <- (T-B)*(b-v^2/(2*a^2))+B^2*v^2/(4*a)
-    return(exp(-A-B*r))
+  B <- (1-exp(-T*a))/a
+  A <- (T-B)*(b-v^2/(2*a^2))+B^2*v^2/(4*a)
+  return(exp(-A-B*r))
 }
 
 # P(Tcanc,Tk) = precioBonoCCVasicek( r(Tcanc), a, b, v, Tk-Tcanc )
@@ -63,14 +71,14 @@ rtcanc = r[(nobs+1),]
 # Creation of P(Tc, Tk) matrix
 for(i in 1:length(x = rtcanc))
 {
+  
+  for(j in 1:(Tvenc - Tcanc))
+  {
     
-    for(j in 1:(Tvenc - Tcanc))
-    {
-        
-        MatrizDto[i,j] = precioBonoCCVasicek(r = rtcanc[i], a = a, b = b, v = v, T = j)
-        
-    }
+    MatrizDto[i,j] = precioBonoCCVasicek(r = rtcanc[i], a = a, b = b, v = v, T = j)
     
+  }
+  
 }
 
 # Creation of coupon payments
@@ -80,7 +88,7 @@ CPay = MatrizDto*c*tau
 # Nominal payment
 N = 1
 
-NomPay = N*rtcanc
+NomPay = N*MatrizDto[,(Tvenc-Tcanc)]
 
 FinalPay = cbind(CPay, NomPay)
 
@@ -90,14 +98,24 @@ RemTc = rowSums(x = FinalPay) # SeRIA EL REM
 Pago = pmin(RemTc, 1)
 # B- CALCULAR PRECIO DE B SUM DE TIPOS DE INTERES MULTIPLICADOS POR DT
 # Este BONO TIENE QUE SER UTILIZANDO EXP(COLSUM)
-Bono = colSums(r[2:(nobs+1),]*dt)
-    
+Bono = exp(x = -colSums(r[2:(nobs+1),]*dt))
+
 # MEAN(A/B)
 
-mean(Pago/Bono)
+mean(Pago/Bono) # Da ya el valor descontado al utilizar el Bono como numerario
 
-P0 = exp(x = -r0*Tcanc)*mean(Pago/Bono)
-P0
 
 # CUPONES UTILIZO VASICEKCC PRECIO DEL BONO P(0,1)*C*TAU + P(0,2)*C*TAU... + P(0,5)*C*TAU
 # P(0,1) SE CALCULA UTILIZANDO LA FORMULA DADA
+
+Dto0_5 = numeric(length = (Tcanc-0))
+
+for (i in 1:length(x = Dto0_5))
+{
+  
+  Dto0_5[i] = precioBonoCCVasicek(r = r0, a = a, b = b, v = v, T = (i))
+  
+}
+
+Cupon0_5 = Dto0_5*c*tau
+sum(Cupon0_5) + mean(Pago/Bono)
